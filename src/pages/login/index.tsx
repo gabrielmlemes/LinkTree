046f9ffa623b699b4040810550b9a158158147c5
Gabriel Lemes <g.moreiralemess@gmail.com>
2024-07-28 19:54:50 -0300

    Adicionando funcionalidade de login à Page Login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,21 +1,34 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Input from "../../components/input";
 import { FormEvent, useState } from "react";
 
-import {auth} from '../../services/firebaseConnection'
+import { auth } from "../../services/firebaseConnection";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate()
 
-  function handleSubmit(e:FormEvent) {
+  function handleSubmit(e: FormEvent) {
     e.preventDefault();
 
-    console.log({
-      email: email,
-      password: password
-    });
-    
+    if (email === "" || password == "") {
+      alert('Preencha todos os campos')
+      return
+    }
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then(()=> {
+      alert('Login realizado com sucesso')
+      navigate('/admin', {replace: true})
+    })
+    .catch((error)=> {
+      console.log('ERRO NO LOGIN ' + error);
+      
+      
+    })
+
   }
 
   return (
